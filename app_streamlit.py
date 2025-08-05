a28b9f03a83c3755ab18cbcc3f4d9fbff9cccb9b
anissffh17 <anissffh17@gmail.com>
2025-08-05 22:34:16 +0800

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -11,18 +11,18 @@ df = pd.read_csv('hasil_prediksi_kelulusan.csv')
 
 # Tabel Mahasiswa + Prediksi Risiko
 st.subheader("Data Mahasiswa + Prediksi Risiko")
-st.dataframe(filtered_df[['Nama Lengkap', 'NIM', 'Prediksi Kelulusan', 'Risiko (%)', 'Faktor Risiko']])
+st.dataframe(df[['Nama Lengkap', 'NIM', 'Prediksi Kelulusan', 'Risiko (%)', 'Faktor Risiko']])
 
 # Pie Chart Rasio Tepat Waktu vs Terlambat
 st.subheader("Rasio Tepat Waktu vs Terlambat")
-rasio = filtered_df['Prediksi Kelulusan'].value_counts().reset_index()
+rasio = df['Prediksi Kelulusan'].value_counts().reset_index()
 rasio.columns = ['Prediksi', 'Jumlah']
 fig_pie = px.pie(rasio, names='Prediksi', values='Jumlah', color_discrete_sequence=px.colors.qualitative.Set1)
 st.plotly_chart(fig_pie, use_container_width=True)
 
 # Top Faktor Risiko (Diambil dari Data Faktor Risiko)
 st.subheader("Top Faktor Risiko")
-faktor_df = filtered_df['Faktor Risiko'].value_counts().reset_index()
+faktor_df = df['Faktor Risiko'].value_counts().reset_index()
 faktor_df.columns = ['Faktor', 'Jumlah']
 fig_bar = px.bar(faktor_df, x='Jumlah', y='Faktor', orientation='h', color='Jumlah', color_continuous_scale='Viridis')
 st.plotly_chart(fig_bar, use_container_width=True)
